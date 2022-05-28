066210c4ddc9f131637c6e3a2d3955d2e67999d8
Tristram Vickers <tristram.vickers@live.com>
2022-05-28 10:14:51 +0800

    Fleshing out database class a little

--- classes/database.class.php
+++ classes/database.class.php
@@ -1,13 +1,20 @@
 <?php 
 
 class Database {
-    //public properties
-    $dsn;
+    //properties
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $dbname='PHPv1';
+    private $dsn;
 
     //private methods
     
     //connect to database
     private function connectDatabase($database){
+        //set dsn
+        $dsn = $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+        //create PDO instance
         try {
             //create new pdo instance
             $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
