97b3bd70dea667619c0278e1b07f3641335640e6
Tristram Vickers <tristram.vickers@live.com>
2022-05-29 08:55:14 +0800

    Yet more changes to database class
    Added some parameters to constructor function, commented out a few sections prior to prepping for a test

--- classes/database.class.php
+++ classes/database.class.php
@@ -2,29 +2,35 @@
 
 class Database {
     //properties
-    private $host = 'localhost';
-    private $user = 'root';
-    private $password = '';
-    private $dbname='PHPv1';
+    // private $host = 'localhost';
+    // private $user = 'root';
+    // private $password = '';
+    // private $dbname='PHPv1';
     private $dsn;
     // private $pdo;
 
     //constructor
-    function __construct() {
-        $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+    function __construct(
+        $host = 'localhost',
+        $dbname='PHPv1',
+        $user = 'root',
+        $password = '',) {
+        //set dsn    
+        $this->dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
     }
     //private methods
     
     //connect to database
     private function connectDatabase($dbname){
         //set dsn
-        $this->dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+        // $this->dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
         //create PDO instance
         try {
-            //create new pdo instance
+            //create new pdo instance (also defining it as variable in here, not as private class property for security I guess)
             $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            //if connection is successful, inform user
             if($pdo) {
-             echo "connected to databse successfully";
+             //echo "connected to databse successfully";
             }
           } catch (PDOException $e) {
               echo $e->getMessage();
