a06e5d9f7192d8b51b4950d1f252ce02a25cc35b
Tristram Vickers <tristram.vickers@live.com>
2022-05-29 15:16:10 +0800

    Updated properties to use $this

--- classes/database.class.php
+++ classes/database.class.php
@@ -12,11 +12,11 @@ class Database {
     //connect to database
     public function connect(){
         //set dsn
-        $this->dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+        $this->dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         //create PDO instance
         try {
             //create new pdo instance (also defining it as variable in here, not as private class property for security I guess)
-            $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            $pdo = new PDO($dsn, $this->user, $this->password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
             //if connection is successful, inform user
             if($pdo) {
              //echo "connected to databse successfully";
